5088fa19da3d343c8d88b7182efcc54544ff0c7a
IamCenie925 <ntxuannhi9205@gmail.com>
2025-09-13 11:38:31 +0700

    prepare for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express"; //const express = require("express")
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -10,16 +11,18 @@ dotenv.config(); //call this method to properly use .env
 
 const app = express();
 const PORT = process.env.PORT || 5001; //if false or undefined, can get 5001 instead
+const __dirname = path.resolve();
 
 //middleware, a function runs in the middle between the request and the respond (before server response to client)
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+if (process.env.NODE_ENV != "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 app.use(express.json()); //this middleware will parse JSON bodies: req.body
 app.use(rateLimiter); //check for rate limiting if user can send a req or should we send an error res
-
 
 //my simple custom middleware
 // app.use((req, res, next) => {
@@ -29,6 +32,14 @@ app.use(rateLimiter); //check for rate limiting if user can send a req or should
 
 //routes
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "..frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   //once database connected, run the application
